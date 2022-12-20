7485cf64d116bd3be430a3522cdcceefa3a9ce68
wasiakaa <aleksandra.wasiak.stud@pw.edu.pl>
2022-12-20 15:42:57 +0100

    Zmieniłam ścieżki do plików test_load

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -6,7 +6,7 @@ from Algorithm.christofides import *
 class MyTestCase(unittest.TestCase):
     def test_load_graph_from_file_1(self):
         # Given
-        filepath = "test_load_1.txt"
+        filepath = "Tests/test_load_1.txt"
         expected_result = {"0": {"1": 1, "2": 2}, "1": {"0": 1, "2": 2}, "2": {"0": 2, "1": 2}}
 
         # When
@@ -17,7 +17,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_load_graph_from_file_2(self):
         # Given
-        filepath = "test_load_2.txt"
+        filepath = "Tests/test_load_2.txt"
         expected_result = {"0": {"1": 1, "2": 2, "3": 3}, "1": {"0": 1, "2": 2, "3": 3}, "2": {"0": 2, "1": 2, "3": 3},
                            "3": {"0": 3, "1": 3, "2": 3}}
 
@@ -29,7 +29,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_load_graph_from_file_3(self):
         # Given
-        filepath = "test_load_3.txt"
+        filepath = "Tests/test_load_3.txt"
         expected_result = {"0": {"1": 1}, "1": {"0": 1}}
 
         # When
