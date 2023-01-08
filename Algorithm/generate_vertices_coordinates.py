6906c6601d3c7abe2b40b7df5b65dcf1a9b2193f
Sabina Majewska <sabinaduchnowska@gmail.com>
2023-01-08 13:28:27 +0100

    Dodałam funkcję, która generuje współrzędne wierzchołków do grafu i funkcję wczytującą taki graf

---
+++ Algorithm/generate_vertices_coordinates.py
@@ -0,0 +1,27 @@
+import random
+
+n = 10 # zadaję liczbę wierzchołków
+moja_lista = []
+number_of_element = 0
+for i in range(n):
+    moja_lista.append([])
+
+while number_of_element < n:
+    x = random.randint(1, 99)
+    y = random.randint(1, 99)
+
+    if not [x, y] in moja_lista:
+        moja_lista[number_of_element].append(x)
+        moja_lista[number_of_element].append(y)
+        number_of_element = number_of_element + 1
+
+
+
+file = "../App/generate_graph_2.txt"
+f = open(file, "w")
+for i in range(n):
+    line = ' '.join([str(moja_lista[i][0]), str(moja_lista[i][1])])
+    f.write(line)
+    f.write("\n")
+
+f.close()
--- App/app_functions.py
+++ App/app_functions.py
@@ -25,6 +25,39 @@ def load_graph_from_file(filepath):
 
     return graph_example
 
+def load_graph_from_file_coordinates(filepath):
+    D = []  # this will be the graph we load
+    file = filepath
+    i = 0
+    with open(file) as f:
+        for line in f.readlines():
+            D.append([])
+            if line[1] != ' ':
+                helper = "".join([line[0], line[1]])
+                D[i].append(int(helper))
+                indeks_y = 3
+            else:
+                D[i].append(int(line[0]))
+                indeks_y = 2
+
+            if line[indeks_y + 1] != '\n':
+                helper = "".join([line[indeks_y], line[indeks_y + 1]])
+                D[i].append(int(helper))
+            else:
+                D[i].append(int(line[indeks_y]))
+            i = i + 1
+
+    global graph_example_1
+    graph_example_1 = D
+
+    # for j in graph_example_1:
+    #     print(j)
+    #
+    # graph_object = graph(graph_example_1, 1)
+    # print(graph_object.G)
+
+    return graph_example_1
+
 
 '''The function takes a graph from the global variable graph_example, calls kruskal on it 
 and saves the result to a file. The format of the saved data is the same as in the input file'''
@@ -32,14 +65,14 @@ and saves the result to a file. The format of the saved data is the same as in t
 
 def save_kruskal_graph_to_file():
     file = "example_out.txt"
-    my_graph = kruskal(graph(graph_example))
+    my_graph = kruskal(graph(graph_example, 1))
     vertices = my_graph.items()
 
     f = open(file, "w")  # "w" means that if the file did not exist it will create it, if it exists it will overwrite it
     for v in vertices:
         edges = v[1].items()
         for e in edges:
-            line = ' '.join([v[0], e[0], e[1]])
+            line = ' '.join([v[0], e[0], e[1]]) # v[0] - wierzchołek startowy; e[0] - wierzchołek docelowy; e[1] - waga
             f.write(line)
             f.write("\n")
 
@@ -50,7 +83,7 @@ def save_kruskal_graph_to_file():
 
 def save_christo_graph_to_file(graph_to_save, filepath):
     file = filepath
-    hamilton = christofides(graph(graph_to_save))
+    hamilton = christofides(graph(graph_to_save, 1))
 
     f = open(file, "w")  # "w" means that if the file did not exist it will create it, if it exists it will overwrite it
     for v in hamilton:
